3a9b7904a96f2a0cd36f0d0449f2282779f75dad
justintjoa <justintoja@gmail.com>
2020-04-24 22:57:42 -0700

    sdf

--- server.py
+++ server.py
@@ -6,11 +6,12 @@ from io import StringIO
 from scapy.all import * 
 
 
-
-a = IP(ttl=10)/ICMP(type=8)
+payload = "A"*10000
+a = IP(ttl=10)/ICMP(type=8)/payload
 a.src = '192.168.222.1'
 a.dst = '192.168.222.2'
 a.version = 4
+a.frag = 0
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,13 +22,18 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('cs177.seclab.cs.ucsb.edu', 44765)
 sock.connect(server_address)
 
-division = 2
+division = 50
+m_len = len(a)
+m_len = m_len.to_bytes(2,'big')
+
+frags = fragment(a,fragsize=500)
 
 while(True):
-    m_len = len(a)
-    m_len = m_len.to_bytes(2,'big')
-    output = m_len + bytes(a)
-    sock.sendall(output)
+    for i in range(division):
+        copy = a
+        copy.frag = i*len(bytes(a))
+        output = m_len + bytes(copy)
+        sock.sendall(output)
     data = sock.recv(100)
     input_length = int.from_bytes(data[0:2], "big") + 2
     while (len(data) < input_length):
